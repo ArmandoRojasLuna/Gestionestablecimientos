9312ae7286a6bc4fdf27790284f57a921885f58e
armando <2020210328@udh.edu.pe>
2024-07-09 22:41:23 -0500

    Añadidas nuevas funcionalidades y mejorado el diseño de la interfaz

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,43 +2,110 @@
 
 @section('content')
 <head>
-    <title>Bienvenido a Gestion de Establecimientos</title>
+    <title>Bienvenido a Gestión de Establecimientos</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #f2f2f2; /* Fondo gris claro */
+        }
+        .card {
+            margin: 10px 0;
+            transition: transform 0.2s;
+            background-size: cover;
+            background-position: center;
+            height: 250px; /* Ajusta según tus necesidades */
+            color: white;
+        }
+        .card:hover {
+            transform: scale(1.05);
+        }
+        .card-body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5); /* Fondo semitransparente */
+        }
+        .card-title {
+            font-size: 1.5rem;
+            font-weight: bold;
+        }
+        .btn {
+            margin-top: 10px;
+        }
+    </style>
 </head>
 <body>
     <div class="container mt-5">
-        <h1 class="text-center">Bienvenido a Gestion de Establecimientos</h1>
+        <h1 class="text-center">Bienvenido a Gestión de Establecimientos</h1>
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="list-group">
-
-
-                <ul class="navbar-nav me-auto">
-    @auth
-        @if(Auth::user()->tipo == "administrador")                        
-            <a href="{{ route('administradores.index') }}" class="list-group-item list-group-item-action">Administradores</a>
-            <a href="{{ route('responsables.index') }}" class="list-group-item list-group-item-action">Responsables</a>
-            <a href="{{ route('establecimientos.index') }}" class="list-group-item list-group-item-action">Establecimientos</a>
-            <a href="{{ route('capacitaciones.index') }}" class="list-group-item list-group-item-action">Capacitaciones</a>
-            <a href="{{ route('comunicaciones.index') }}" class="list-group-item list-group-item-action">Comunicados</a>
-        
-            @else
-            <a href="{{ route('capacitaciones.index') }}" class="list-group-item list-group-item-action">Capacitaciones</a>
-            <a href="{{ route('comunicaciones.index') }}" class="list-group-item list-group-item-action">Comunicados</a>
-
-            
-
-           
-        @endif
-    @endauth
-</ul>
-
-
-
+                <div class="row">
+                    @auth
+                        @if(Auth::user()->tipo == "administrador")                        
+                            <div class="col-md-6">
+                                <div class="card" style="background-image: url('{{ asset('images/administradores.jpg') }}');">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Administradores</h5>
+                                        <a href="{{ route('administradores.index') }}" class="btn btn-primary">Gestionar</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card" style="background-image: url('{{ asset('images/responsables.jpg') }}');">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Responsables</h5>
+                                        <a href="{{ route('responsables.index') }}" class="btn btn-primary">Gestionar</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card" style="background-image: url('{{ asset('images/establecimientos.jpg') }}');">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Establecimientos</h5>
+                                        <a href="{{ route('establecimientos.index') }}" class="btn btn-primary">Gestionar</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card" style="background-image: url('{{ asset('images/capacitaciones.jpg') }}');">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Capacitaciones</h5>
+                                        <a href="{{ route('capacitaciones.index') }}" class="btn btn-primary">Gestionar</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card" style="background-image: url('{{ asset('images/comunicados.jpg') }}');">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Comunicados</h5>
+                                        <a href="{{ route('comunicaciones.index') }}" class="btn btn-primary">Gestionar</a>
+                                    </div>
+                                </div>
+                            </div>
+                        @else
+                            <div class="col-md-6">
+                                <div class="card" style="background-image: url('{{ asset('images/capacitaciones.jpg') }}');">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Capacitaciones</h5>
+                                        <a href="{{ route('capacitaciones.index') }}" class="btn btn-primary">Gestionar</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card" style="background-image: url('{{ asset('images/comunicados.jpg') }}');">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Comunicados</h5>
+                                        <a href="{{ route('comunicaciones.index') }}" class="btn btn-primary">Gestionar</a>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+                    @endauth
                 </div>
             </div>
         </div>
     </div>
 </body>
-
 @endsection
